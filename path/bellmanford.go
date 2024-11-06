b39e9e3256d5e100317da79283438cc6cf5d9530
mcaci <michele.caci@gmail.com>
2024-11-06 23:57:13 +0100

    simplify shortest path

--- path/bellmanford.go
+++ path/bellmanford.go
@@ -10,24 +10,18 @@ type Weighter interface{ Weight() int }
 
 func BellmanFordDist[T comparable](g graph.Graph[T], s *graph.Vertex[T]) map[*graph.Vertex[T]]*Distance[T] {
 	d := make(map[*graph.Vertex[T]]*Distance[T])
-	vs := g.Vertices()
-	for i := range vs {
-		switch v := vs[i]; v {
-		case s:
-			d[v] = &Distance[T]{v: s, u: v, d: 0}
-		default:
-			d[v] = &Distance[T]{v: s, u: v, d: math.MaxInt}
+	for _, v := range g.Vertices() {
+		d[v] = &Distance[T]{v: s, u: v}
+		if v != s {
+			d[v].d = math.MaxInt
 		}
 	}
 	canRelax := func(x, y *graph.Vertex[T], w Weighter) bool {
 		return d[x].d+w.Weight() < d[y].d && d[x].d+w.Weight() > 0
 	}
-	relax := func(x, y *graph.Vertex[T], w Weighter) {
-		d[y].setDistance(w.Weight() + d[x].d)
-	}
-	es := g.Edges()
-	for range vs {
-		for _, e := range es {
+	relax := func(x, y *graph.Vertex[T], w Weighter) { d[y].setDistance(w.Weight() + d[x].d) }
+	for range g.Vertices() {
+		for _, e := range g.Edges() {
 			w := e.P.(Weighter)
 			switch {
 			case canRelax(e.X, e.Y, w):
--- path/shortest.go
+++ path/shortest.go
@@ -6,17 +6,16 @@ func Shortest[T comparable](g graph.Graph[T], d map[*graph.Vertex[T]]*Distance[T
 	if len(g.Vertices()) < 2 {
 		return nil
 	}
-	path := []*graph.Vertex[T]{y}
-	v := y
 	isShortestDist := func(u, v *graph.Vertex[T], w Weighter) bool { return d[u].d+w.Weight() == d[v].d }
 	isConnectingEdge := func(u, v *graph.Vertex[T], e *graph.Edge[T]) bool {
 		return (e.X == u && e.Y == v) || (e.X == v && e.Y == u)
 	}
-	es := g.Edges()
+	path := []*graph.Vertex[T]{y}
+	v := y
 	for v != x {
 	neighbourSearch:
 		for _, u := range g.AdjacentNodes(v) {
-			for _, edge := range es {
+			for _, edge := range g.Edges() {
 				if !isConnectingEdge(u, v, edge) {
 					continue
 				}
